3c6032320e5e5ac49f577e50b26347b576c6643d
Misash <misash@outlook.com>
2022-04-03 21:58:27 -0500

    first commit

---
+++ lab_01/server.cpp
@@ -0,0 +1,49 @@
+
+#include<iostream>
+
+/*standard symbols */
+#include <unistd.h>  
+
+/* sockets */
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#include <arpa/inet.h>
+
+/* strings / errors*/
+#include <errno.h>
+#include <stdio.h> 
+#include <string.h> 
+
+/* server parameters */
+#define SERV_PORT       8080              /* port */
+#define SERV_HOST_ADDR "192.168.0.21"     /* IP, only IPV4 support  */
+#define BUF_SIZE        100               /* Buffer rx, tx max size  */
+#define BACKLOG         5                 /* Max. client pending connections  */
+
+
+using namespace std;
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
+
+
+
+
+
